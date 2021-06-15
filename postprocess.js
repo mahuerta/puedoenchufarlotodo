97a9b1f18d394538401e462a79aab1c49c85414b
mahuerta <miguel5692@gmail.com>
2021-06-15 20:34:02 +0200

    fix: demon sudo

--- postprocess.js
+++ postprocess.js
@@ -2,17 +2,6 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 const filename = 'price.json';
 const json = await readJSON(filename)
-const NATIONAL_HOLIDAYS = [
-        "01/01/2021",
-        "06/01/2021",
-        "02/04/2021",
-        "01/05/2021",
-        "12/10/2021",
-        "01/11/2021",
-        "06/12/2021",
-        "08/12/2021",
-        "25/12/2021",
-      ];
 
 const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
   const getFirstHour = Hora.split('-')[0];
@@ -27,6 +16,17 @@ const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
 });
 
 function getZone(hour,date,day) {
+  const NATIONAL_HOLIDAYS = [
+        "01/01/2021",
+        "06/01/2021",
+        "02/04/2021",
+        "01/05/2021",
+        "12/10/2021",
+        "01/11/2021",
+        "06/12/2021",
+        "08/12/2021",
+        "25/12/2021",
+      ];
   if (hour >= 0 && hour < 8 
         || day === 6 
         || day === 0
